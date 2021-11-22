3a2553bf980cfd38e25798d1d6fb5b41aad2e2c1
Emil Christiansen <emilsen68@gmail.com>
2021-11-23 00:59:29 +0100

    Learning about websockets
    Just uploading some code for myself.
    
    This will be morphed into the actual code later

---
+++ Server/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+// Reading input from terminal start
+const port = parseInt(process.argv[2])
+console.log(`${port} registered as server port`)
+// Reading input from terminal end
+
+app.use(cors()) // Making sure the browser can request more data after it is loaded on the client computer.
+app.use(express.urlencoded({extended:false}))
+//app.use(cookieParser()) // Middleware for handling cookies
+//app.use(upload()) // Fileupload system
+
+app.use("/static", express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/html/index.html'))
+})
+
+io.on('connection', (socket) => {
+    console.log('A user connected')
+    socket.broadcast.emit("Hello")
+
+    socket.on("chat message", (msg) => {
+        console.log(`Message: ${msg}`)
+        io.emit("chat message", msg)
+    })
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected")
+    })
+})
+
+server.listen(port, () => {console.log(`listening on ${port}`)})
