5bbabd79c8f0c7754f1d98495348a6d790c8dd61
Minyguy <benjaminleikvold@gmail.com>
2021-11-23 11:51:33 +0100

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -5,7 +5,12 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
+const { captureRejectionSymbol } = require('events')
 const io = new Server(server)
+var users = {}
+
+const rooms = io.of("/").adapter.rooms;
+const sids = io.of("/").adapter.sids;
 
 // Reading input from terminal start
 const port = parseInt(process.argv[2])
@@ -23,19 +28,34 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/html/index.html'))
 })
 
-io.on('connection', (socket) => {
-    console.log('A user connected')
-    socket.broadcast.emit("chat message", "A user has connected")
+// Socket is basically user link
 
+io.on('connection', (socket) => {
+    const userId = await fetchUserId(socket);
+    console.log(`User ${userID} has joined`)
+    socket.broadcast.emit("chat message", `user ${userID} has connected`)
+    users[socketId] = "testname"
     socket.on("chat message", (msg) => {
         console.log(`Message: ${msg}`)
-        io.emit("chat message", msg)
+        io.emit("chat message", `${userID}: ${msg}`)
     })
 
     socket.on('disconnect', () => {
-        console.log("A user disconnected")
+        console.log(`User ${userID} disconnected`)
         socket.broadcast.emit("chat message", "A user has disconnected")
     })
-})
+
+    //Event when user clicks the play button after giving a roomname
+    // socket.on('joinRoom', () => {
+    //     console.log("User joined a room")
+    //     if room in sessions {
+    //         //If the roomname exists, check if full, otherwise join it
+    //     }
+    //     else {
+    //         //If the roomname doesnt exist, create it.
+    //     }
+    //     socket.join(room)
+    // })
+}
 
 server.listen(port, () => {console.log(`listening on ${port}`)})
