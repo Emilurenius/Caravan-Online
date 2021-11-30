aa53a560a4d3a28f082133fd36d8d6f721533c18
Minyguy <benjaminleikvold@gmail.com>
2021-11-30 12:00:41 +0100

    stuff

--- Server/public/javascript/socketio.js
+++ Server/public/javascript/socketio.js
@@ -5,8 +5,9 @@ const changeNameForm = document.getElementById("changeNameForm")
 
 const submitButton = document.getElementById('submitButton')
 const input = document.getElementById('input')
-const nameInput = document.getElementById('name')
+const nameInput = document.getElementById('nameInput')
 const nameButton = document.getElementById('nameChangeButton')
+
 var name = ''
 
 socket.on('connect', () => {
@@ -16,6 +17,7 @@ socket.on('connect', () => {
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     if (input.value) {
+        console.log("chatmessage")
         socket.emit("chat message", input.value)
         input.value = ''
     }
@@ -23,6 +25,7 @@ messageForm.addEventListener('submit', (e) => {
 
 changeNameForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    console.log("Trying to change name")
     if (nameInput.value) {
         socket.emit("changeName", nameInput.value)
         console.log(`${name} changed name to ${nameInput.value}`)
